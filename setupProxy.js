cbe27bd5a63ca84993640c7ccd2e1326b56e17ff
jerome cnockaert <47606138+jeje59223@users.noreply.github.com>
2022-01-09 18:17:30 +0100

    add http proxy middleware (#1)
    * add http proxy middleware
    
    * add http proxy middleware
    
    * refacto

---
+++ setupProxy.js
@@ -0,0 +1,21 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+// Switch to true to see logs about proxy rewrite
+const DEBUG = true;
+
+module.exports = (app) => {
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  function defineProxy(apiUrl, reactUri) {
+    console.log(`${apiUrl} -> ${reactUri}`);
+    const proxyOptions = {
+      target: apiUrl,
+      changeOrigin: true,
+      secure: false,
+      pathRewrite: { [`^${reactUri}`]: '' },
+      logLevel: DEBUG ? 'debug' : 'info',
+    };
+    app.use(createProxyMiddleware(reactUri, proxyOptions));
+  }
+  defineProxy(process.env.BBF_VIDEO_GAMES, '/api');
+};
