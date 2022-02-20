68dd2b6d94c046348fa5540886417821e22c070f
jeje59223 <jcnockaert@hotmail.fr>
2022-02-20 18:19:26 +0100

    correction package.json script test and correction circleRating test

--- src/components/atoms/CircleRating/CircleRating.test.tsx
+++ src/components/atoms/CircleRating/CircleRating.test.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import {act} from 'react-dom/test-utils';
-import * as ReactDOM from "react-dom";
 import { generateClassName } from './generateClassName';
 import { convertsRatingToPercent } from "./convertsRatingToPercent";
 
 describe('<CircleRating>', () => {
     test('should return percentBad when a rating is less than 50', function () {
         const colorRating = generateClassName(40);
-        expect(colorRating).toEqual('percentBad');
+        expect(colorRating).toEqual('percent-bad');
     });
     test('should return percentMedium when a rating is less than 70 and greater than or equal to 50', function () {
         const colorRating = generateClassName(60);
-        expect(colorRating).toEqual('percentMedium');
+        expect(colorRating).toEqual('percent-medium');
     });
     test('should return percentGood when a rating is less than 85 and greater than or equal to 70', function () {
         const colorRating = generateClassName(75);
-        expect(colorRating).toEqual('percentGood');
+        expect(colorRating).toEqual('percent-good');
     });
     test('should return percentGood when a rating is greater or equal than 85', function () {
         const colorRating = generateClassName(87);
-        expect(colorRating).toEqual('percentVeryGood');
+        expect(colorRating).toEqual('percent-very-good');
     });
     test('should return a rating to percent', () => {
         const convertsRating = convertsRatingToPercent(1);
