047a2b43dc0ba1bda08302ec4e18f0494fcc7ad7
Jerome <jerome.cnockaert@leroymerlin.fr>
2022-05-16 12:15:06 +0200

    correction after Flo Barriol review's

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import './App.css';
 import { BrowserRouter as Router, useRoutes } from 'react-router-dom';
 import NavBar, { pages } from './components/molecules/NavBar/NavBar';
 
+const routes = pages.map((page) => {
+  return {
+    path: page.url,
+    element: page.element,
+  };
+});
+
 const AppRoute = () => {
-  return useRoutes([
-    { path: pages[0].url, element: pages[0].element },
-    { path: pages[1].url, element: pages[1].element },
-    { path: pages[2].url, element: pages[2].element },
-    { path: pages[3].url, element: pages[3].element },
-  ]);
+  return useRoutes(routes);
 };
 
 const App = () => {
